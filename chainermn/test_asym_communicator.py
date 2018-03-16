f39161ff9c485792e1fa0bd4ace8d0ddd2a312c6
Yohei Tsuji <t.t.t.yohei@gmail.com>
2018-03-16 15:25:22 +0900

    Update

---
+++ chainermn/test_asym_communicator.py
@@ -0,0 +1,41 @@
+import chainermn
+from mpi4py import MPI
+
+import common
+
+
+def _split(arr, n):
+    N = len(arr)
+    for i in range(n):
+        head = (i * N) // n
+        tail = ((i + 1) * N) // n
+        yield arr[head:tail]
+        
+
+
+def main():
+    comm = chainermn.create_communicator('naive')
+
+    ratio = 0.2
+    if comm.size * ratio >= comm.size:
+        n_masters = comm.inter_size
+        if comm.intra_rank == 0:
+            is_master = True
+        else:
+            is_master = False
+    else:
+        n_masters = int(comm.size * ratio)
+        nodes = list(range(comm.inter_size))
+        chunked_nodes = list(_split(nodes, n_masters))
+        is_master = False
+        for chunk in chunked_nodes:
+            if comm.inter_rank == chunk[0] and comm.intra_rank == 0:
+                is_master = True
+
+    print_mpi = common.create_print_mpi(comm.mpi_comm)
+    print_mpi('is master: {}'.format(is_master))
+
+
+
+if __name__ == '__main__':
+    main()
