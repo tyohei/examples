3033b09c1a0933281ca6520f898928cf2ab17572
Yohei Tsuji <t.t.t.yohei@gmail.com>
2018-04-14 16:31:59 +0900

    Revised common.py

--- mpi/python/common.py
+++ mpi/python/common.py
@@ -1,30 +1,31 @@
-from mpi4py import MPI
+from socket import gethostname
 
 
 def create_print_mpi(comm):
     rank = comm.rank
     size = comm.size
-    host = MPI.Get_processor_name()
+    host = gethostname()
     digits = len(str(size - 1))
     prefix = '[{{:0{}}}/{}:{}] '.format(digits, size, host).format(rank)
 
-    def print_mpi(obj, root=None):
+    def print_mpi(*args, root=None, **kwargs):
         for i in range(size):
             if i == rank:
                 if root is not None:
                     if i == root:
                         print(prefix, end='')
-                        print(obj)
+                        print(*args, **kwargs)
                 else:
                     print(prefix, end='')
-                    print(obj)
+                    print(*args, **kwargs)
             comm.Barrier()
     return print_mpi
 
 
 def main():
+    from mpi4py import MPI
     print_mpi = create_print_mpi(MPI.COMM_WORLD)
-    print_mpi('Hello World!')
+    print_mpi('Hello World!', 'yes')
 
 
 if __name__ == '__main__':
