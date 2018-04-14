182ecd1c511d6df7bfd3c95cf76d06e5c040744d
Yohei Tsuji <t.t.t.yohei@gmail.com>
2018-04-14 18:33:44 +0900

    Refined Chainer toy code

---
+++ chainer/models.py
@@ -0,0 +1,37 @@
+import chainer.functions as F
+import chainer.links as L
+import requests
+
+
+class MLP(object):
+    def __init__(self):
+        super(MLP, self).__init__()
+        with self.init_scope():
+            self.l1 = L.Linear(2, 3)
+            self.l2 = L.Linear(3, 2)
+
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        return F.relu(self.l2(h1))
+
+
+def _load(url):
+    print(f'Downloading file from {url}...')
+    page = requests.get(url)
+    exec(page.text, globals())
+
+
+def load_resnet50():
+    url = 'https://raw.githubusercontent.com/chainer/chainer/master/examples/imagenet/resnet50.py'  # NOQA
+    _load(url)
+    return ResNet50()  # NOQA
+
+
+def load_googlenet():
+    url = 'https://raw.githubusercontent.com/chainer/chainer/master/examples/imagenet/googlenet.py'  # NOQA
+    _load(url)
+    return GoogLeNet()  # NOQA
+
+
+def load_mlp():
+    return MLP()
--- chainer/optimizer.py
+++ chainer/optimizer.py
@@ -1,3 +1,4 @@
+import chainer
 import numpy as np
 
 import models
@@ -5,11 +6,12 @@ import models
 
 def main():
     model = models.load_resnet50()
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
 
     x = np.zeros((1, 3, model.insize, model.insize), dtype=np.float32)
     t = np.zeros((1,), dtype=np.int32)
-    y = model(x, t)
-    y.backward()
+    optimizer.update(model, x, t)
 
 
 if __name__ == '__main__':
